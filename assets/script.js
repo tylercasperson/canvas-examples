0babb44d501861c1c63889edee9e2ca9d11eaea8
Tyler Casperson <tylercasperson@gmail.com>
2021-11-02 20:47:08 -0600

    particles cursor follow effect

--- assets/script.js
+++ assets/script.js
@@ -1,112 +1,76 @@
-let canvas;
-let ctx;
-let flowField;
-let flowFieldAnimation;
-
-window.onload = function () {
-  canvas = document.getElementById('canvas');
-  ctx = canvas.getContext('2d');
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-  flowField = new FlowFieldEffect(ctx, canvas.width, canvas.height);
-  flowField.animate(0);
-};
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+const particlesArray = [];
+let hue = 0;
 
 window.addEventListener('resize', function () {
-  cancelAnimationFrame(flowFieldAnimation);
-  canvas.width = window.innerWidth;
+  canvas.width = window.innerHeight;
   canvas.height = window.innerHeight;
-  flowField = new FlowFieldEffect(ctx, canvas.width, canvas.height);
-  flowField.animate(0);
 });
 
 const mouse = {
-  x: 0,
-  y: 0,
+  x: undefined,
+  y: undefined,
 };
-window.addEventListener('mousemove', function (e) {
+canvas.addEventListener('click', function (e) {
   mouse.x = e.x;
   mouse.y = e.y;
+  for (let i = 0; i < 10; i++) {
+    particlesArray.push(new Particle());
+  }
 });
 
-class FlowFieldEffect {
-  #ctx;
-  #width;
-  #height;
-  constructor(ctx, width, height) {
-    this.#ctx = ctx;
-    this.#ctx.lineWidth = 1;
-    this.#width = width;
-    this.#height = height;
-    this.lastTime = 0;
-    this.interval = 1000 / 60;
-    this.timer = 0;
-    this.cellSize = 15;
-    this.gradient;
-    this.#createGradient();
-    this.#ctx.strokeStyle = this.gradient;
-    this.radius = 0;
-    this.vr = 0.03;
+canvas.addEventListener('mousemove', function (e) {
+  mouse.x = e.x;
+  mouse.y = e.y;
+  for (let i = 0; i < 50; i++) {
+    particlesArray.push(new Particle());
   }
-  #createGradient() {
-    this.gradient = this.#ctx.createLinearGradient(
-      0,
-      0,
-      this.#width,
-      this.#height
-    );
-    this.gradient.addColorStop('0.1', '#ff5c33');
+});
 
-    this.gradient.addColorStop('0.2', '#ff66b3');
-    this.gradient.addColorStop('0.4', '#ccccff');
-    this.gradient.addColorStop('0.6', '#b3ffff');
-    this.gradient.addColorStop('0.8', '#80ff80');
-
-    this.gradient.addColorStop('0.9', '#ffff33');
+class Particle {
+  constructor() {
+    this.x = mouse.x;
+    this.y = mouse.y;
+    // this.x = Math.random() * canvas.width;
+    // this.y = Math.random() * canvas.height;
+    this.size = Math.random() * 15 + 1;
+    this.speedX = Math.random() * 3 - 1.5;
+    this.speedY = Math.random() * 3 - 1.5;
+    this.color = 'hsl(' + hue + ', 100%, 50%)';
   }
-  #drawLine(angle, x, y) {
-    let positionX = x;
-    let positionY = y;
-    let dx = mouse.x - positionX;
-    let dy = mouse.y - positionY;
-    let distance = dx * dx + dy * dy;
-    if (distance > 600000) {
-      distance = 600000;
-    } else if (distance < 50000) {
-      distance = 50000;
-    }
-
-    const length = distance / 10000;
-    this.#ctx.beginPath();
-    this.#ctx.moveTo(x, y);
-    this.#ctx.lineTo(
-      x + Math.cos(angle) * length,
-      y + Math.sin(angle) * length
-    );
-    this.#ctx.stroke();
+  update() {
+    this.x += this.speedX;
+    this.y += this.speedY;
+    if (this.size > 0.2) this.size -= 0.1;
   }
-  animate(timeStamp) {
-    const deltaTime = timeStamp - this.lastTime;
-    this.lastTime = timeStamp;
-    if (this.timer > this.interval) {
-      this.#ctx.clearRect(0, 0, this.#width, this.#height);
-      this.radius += this.vr;
-      if (this.radius > 5 || this.radius < -5) this.vr *= -1;
-
-      for (let y = 0; y < this.#height; y += this.cellSize) {
-        for (let x = 0; x < this.#width; x += this.cellSize) {
-          const angle =
-            (Math.cos(mouse.x * x * 0.00001) +
-              Math.sin(mouse.y * y * 0.00001)) *
-            this.radius;
-          this.#drawLine(angle, x, y);
-        }
-      }
-
-      this.timer = 0;
-    } else {
-      this.timer += deltaTime;
-    }
-    flowFieldAnimation = requestAnimationFrame(this.animate.bind(this));
+  draw() {
+    ctx.fillStyle = this.color;
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+    ctx.fill();
   }
 }
+
+function handleParticles() {
+  for (let i = 0; i < particlesArray.length; i++) {
+    particlesArray[i].update();
+    particlesArray[i].draw();
+    if (particlesArray[i].size <= 0.3) {
+      particlesArray.splice(i, 1);
+      i--;
+    }
+  }
+}
+
+function animate() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  //   ctx.fillStyle = 'rgba(0,0,0,0.02)';
+  //   ctx.fillRect(0, 0, canvas.width, canvas.height);
+  handleParticles();
+  hue += 2;
+  requestAnimationFrame(animate);
+}
+animate();
